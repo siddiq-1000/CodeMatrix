2acd6a0233555c48372db29ec8734b66e129b16a
NITHYA123-hub <nithyagowda367@gmail.com>
2025-07-19 21:01:42 +0530

    Delete CV2025/Mini Social media app directory

--- CV2025/Mini Social media app/Mini Social media app/modules/comment_post.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import simpledialog
-
-def comment_window(root, post):
-    comment = simpledialog.askstring("Comment", "Write your comment:")
-    if comment:
-        post["comments"].append({"user": "Nidhi", "text": comment})
--- CV2025/Mini Social media app/modules/like_post.py
+++
@@ -1,2 +0,0 @@
-def like(post):
-    post['likes'] += 1
